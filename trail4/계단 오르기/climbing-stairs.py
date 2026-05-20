3255d4b2b27a12550bd4d8a3b9d784a10c2bca4e
leesy010504 <148897132+leesy010504@users.noreply.github.com>
2026-05-20 13:57:54 +0900

    [Passed] 계단 오르기 (51ms, 16MB)

---
+++ trail4/계단 오르기/climbing-stairs.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+dp = [0] * max(4, n + 1)
+
+dp[0] = 1 
+dp[1] = 0
+dp[2] = 1
+dp[3] = 1
+
+for i in range(4, n + 1):
+    dp[i] = (dp[i - 2] + dp[i - 3]) % 10007
+
+print(dp[n])
